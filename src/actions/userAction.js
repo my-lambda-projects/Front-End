f793904a12128972a5af27c9d0dfdff09ba1c3d3
bgoonz <bryan.guner@gmail.com>
2021-05-26 10:53:59 -0400

    my branch

---
+++ src/actions/userAction.js
@@ -0,0 +1,41 @@
+
+import axios from 'axios';
+
+export const LOGIN_LOADING = 'LOGIN_LOADING';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAILURE = 'LOGIN_FAILURE';
+
+export const REGISTER_LOADING = 'REGISTER_LOADING';
+export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
+export const REGISTER_FAILURE = 'REGISTER_FAILURE';
+
+export const USER_LOGOUT = 'USER_LOGOUT';
+
+export const registerUser = (regCreds) => (dispatch) => {
+  dispatch({ type: REGISTER_LOADING });
+  axios
+    .post('https://ft-potluck-planner-5.herokuapp.com/api/auth/register', regCreds)
+    .then((res) => dispatch({ type: REGISTER_SUCCESS, payload: res.data }))
+    .catch((err) => {
+      dispatch({ type: REGISTER_FAILURE, payload: err.response.data.message });
+    });
+};
+
+export const loginUser = (loginCreds) => (dispatch) => {
+  window.localStorage.removeItem('token');
+  dispatch({ type: LOGIN_LOADING });
+  axios
+    .post('https://ft-potluck-planner-5.herokuapp.com/api/auth/login', loginCreds)
+    .then((res) => {
+      window.localStorage.setItem('token', res.data.token);
+      dispatch({ type: LOGIN_SUCCESS, payload: res.data });
+    })
+    .catch((err) => {
+      dispatch({ type: LOGIN_FAILURE, payload: err.response.data.message });
+    });
+};
+
+export const logoutUser = () => {
+  window.localStorage.removeItem('token');
+  return { type: USER_LOGOUT };
+};
---
+++ src/reducers/userReducer.js
@@ -0,0 +1,69 @@
+import {
+    REGISTER_LOADING,
+    REGISTER_SUCCESS,
+    REGISTER_FAILURE,
+    LOGIN_LOADING,
+    LOGIN_SUCCESS,
+    LOGIN_FAILURE,
+  } from  '../actions/userAction';
+  
+  const initialState = {
+    username: '',
+    password: '',
+    email: '',
+    error: '',
+    isLoading: false,
+    user_id: '',
+    token: window.localStorage.getItem('token') || '',
+    message: '',
+    isLoggedIn: false,
+  };
+  
+  export const userReducer = (state = initialState, action) => {
+    switch (action.type) {
+      case REGISTER_LOADING:
+        return {
+          ...state,
+          isLoading: true,
+        };
+      case REGISTER_SUCCESS:
+        return {
+          ...state,
+          username: action.payload.username,
+          password: action.payload.password,
+          email: action.payload.email,
+          isLoading: false,
+        };
+      case REGISTER_FAILURE:
+        return {
+          ...state,
+          isLoading: false,
+          error: action.payload,
+        };
+      case LOGIN_LOADING:
+        return {
+          ...state,
+          isLoading: true,
+          error: '',
+        };
+      case LOGIN_SUCCESS:
+        return {
+          ...state,
+          isLoading: false,
+          username: action.payload.username,
+          user_id: action.payload.user_id,
+          token: action.payload.token,
+          message: action.payload.message,
+          isLoggedIn: true,
+        };
+      case LOGIN_FAILURE:
+        return {
+          ...state,
+          isLoading: false,
+          error: action.payload,
+        };
+  
+      default:
+        return state;
+    }
+  };
