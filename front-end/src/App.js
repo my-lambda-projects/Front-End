b84372f9f33e61883d75b7722c9277b697bfea00
schandrupatla <55516943+schandrupatla@users.noreply.github.com>
2021-05-25 18:48:46 -0700

    added useReducer,userActions,Signup

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,26 @@
 import './App.css';
+import {  BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
+import PrivateRoute from './utils/PrivateRoute'
+import Login from './components/Login'
+import Registration from './components/Registration'
+import Invite from './components/Invite'
 
 function App() {
+
+
   return (
+    <>
     <div className="App">
- <h1>Welcome to Potluck-planner</h1>
+    <h1>Welcome to Potluck-planner</h1>
     </div>
+    <Router>
+    <Switch>
+      <PrivateRoute exact path="/protected" component={Invite}/>
+      <Route path="/login" component={Login} />
+      <Route path="/Registration" component={Registration} />
+    </Switch>
+    </Router>
+    </>
   );
 }
 
