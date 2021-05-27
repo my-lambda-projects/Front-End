087c9f942f917da9a7429ce5ca729cdc17621324
bgoonz <bryan.guner@gmail.com>
2021-05-27 18:15:38 -0400

    Guest

--- makefile
+++ makefile
@@ -1,5 +1,5 @@
 git:
 	git add .
 	git commit -m "$m-update"
-	git push -u origin sandbox
+	git push -u origin bryan-guner
 # just type make into the terminal to exicute these commands!
--- src/components/Guest.js
+++ src/components/Guest.js
@@ -1,97 +1,24 @@
 import React from 'react';
-import moment from 'moment';
-import {Header, Icon, Container, List, Card,} from 'semantic-ui-react';
+import {
+  Container,
+  Card,
+  CardBody,
+  CardHeader,
+  CardText,
+  Col,
+  CardImg,
+  Button,
+  Row,
+} from 'reactstrap';
 
-const Guest = ({event, eventTime, /*user_id*/}) => {
+const Guest = props => {
+  const {users} = props;
+
   return (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        fontFamily: 'Poppins',
-      }}
-    >
-      <Container textAlign="center">
-        <Header as="h2" icon textAlign="center" style={{fontSize: '3rem'}}>
-          <Icon name="calendar check" />
-          {event.data.event_name}
-        </Header>
-      </Container>
-      <Container textAlign="center">
-        <List style={{fontSize: '2rem'}}>
-          <List.Item>
-            <List.Icon name="calendar" />
-            {moment (event.data.date).format ('LL')}
-          </List.Item>
-          <List.Item>
-            <List.Icon name="map marker" />
-            {event.data.address}, {event.data.city}, {event.data.state}{' '}
-          </List.Item>
-          <List.Item>
-            <List.Icon name="outline clock" />
-            {`${eventTime}`}
-          </List.Item>
-          <List.Item>
-            <List.Icon name="newspaper outline" />
-            {event.data.description}
-          </List.Item>
-        </List>
-      </Container>
-      <Card.Group centered style={{margin: '30px'}}>
-        <Card style={{padding: '20px'}}>
-          <Card.Header textAlign="center" style={{fontSize: '2.5rem'}}>
-            Guests
-          </Card.Header>
-          <Card.Content>
-            <List style={{margin: 'auto'}}>
-              {event.data.guests.map (guest => {
-                //Mapping over guests to display
+    <CardBody>
+      <CardText>{users.username}</CardText>
 
-                if (guest.attending) {
-                  return (
-                    <List.Item key={guest.user_id} style={{fontSize: '2rem'}}>
-                      {guest.full_name} - Attending{' '}
-                    </List.Item>
-                  );
-                } else {
-                  return (
-                    <List.Item key={guest.user_id} style={{fontSize: '2rem'}}>
-                      {guest.full_name} - Invited
-                    </List.Item>
-                  );
-                }
-              })}
-            </List>
-          </Card.Content>
-        </Card>
-        <Card style={{padding: '20px'}}>
-          <Card.Header textAlign="center" style={{fontSize: '2.5rem'}}>
-            Choose a Dish
-          </Card.Header>
-          <Card.Content>
-            <List style={{margin: 'auto'}}>
-              {' '}
-              {typeof event.data.recipes === 'string'
-                ? <List.Item>{event.data.recipes}</List.Item>
-                : event.data.recipes.map (recipe => {
-                    //Determine if Recipes is an array or string and return value
-                    return (
-                      <List.Item
-                        key={recipe.recipe_name}
-                        style={{fontSize: '2rem'}}
-                      >
-
-                        {/* Toggling between the name and unclaimed */}
-
-                      </List.Item>
-                    );
-                  })}
-            </List>
-          </Card.Content>
-        </Card>
-      </Card.Group>
-    </div>
+    </CardBody>
   );
 };
 
